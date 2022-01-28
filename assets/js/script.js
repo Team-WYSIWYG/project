a6332d62a58ceb52f1ded242eaee60ec2656928e
Aidan Chamberlain <asunshine99@gmail.com>
2022-01-27 19:39:04 -0600

    css updates, updated search history functionality, set initial conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // VARIABLES
 let selectForm = document.querySelector("#countryForm");
 let selectBar = document.querySelector("#country-select");
+let countryInit = "US";
 
 // FUNCTIONS
 // country api call
@@ -24,7 +25,6 @@ function fetchCountries() {
         .then((response) => response.json())
         // Return JSON response CREAM FILLING
         .then((countries) => {
-            console.log(countries);
             // adds value to options
             countries.forEach((country) => {
                 let countryRow = document.createElement("option");
@@ -60,9 +60,14 @@ function fetchChosenCountryData(countryIso) {
     fetch(fetchCountriesURL, requestOptions)
         .then((response) => response.json())
         .then((result) => {
-            console.log(result);
             // add item to search history
             document.getElementById("lastChosenCountry").innerHTML += `<div data-country="US"> ${result.name}</div>`;
+            let numb = document.getElementById("lastChosenCountry").childElementCount;
+            if (numb > 3) {
+                // remove child of lastchosencountry at child index of 0
+                let searchHistory = document.getElementById("lastChosenCountry");
+                searchHistory.removeChild(searchHistory.childNodes[0]);
+            }
             // sends result data to currency rate function
             populateCountryData(result);
         })
@@ -85,13 +90,56 @@ function fetchFlag() {
         .then((result) => {
             // dislpays a loading message while api is fetching flag data
             document.getElementById("loadingMessage").innerHTML = "";
-            console.log(result);
             // creates URL to be used as image element src attribute
             let flagURL = URL.createObjectURL(result);
             // adds image to page
             document.getElementById("showFlag").innerHTML = `<img class="border-4 rounded" src="${flagURL}" >`;
         })
         // if nothing is pulled, error is displayed
+        .catch((error) => console.log("error", error));
+}
+
+function fetchUSA_Flag() {
+    // displays "loading" in between user choices
+    document.getElementById("loadingMessage").innerHTML = `<div>loading</div>`;
+    // Telling countryIso to equal the selected option from the API array "data-iso2"
+    // template literal using country iso code to tell api which flag to get
+    let fetchUSA_URL = `https://countryflagsapi.com/png/US`;
+
+    fetch(fetchUSA_URL)
+        .then((response) => response.blob())
+        .then((result) => {
+            // dislpays a loading message while api is fetching flag data
+            document.getElementById("loadingMessage").innerHTML = "";
+            // creates URL to be used as image element src attribute
+            let flagURL = URL.createObjectURL(result);
+            // adds image to page
+            document.getElementById("showFlag").innerHTML = `<img class="border-4 rounded" src="${flagURL}" >`;
+        })
+        // if nothing is pulled, error is displayed
+        .catch((error) => console.log("error", error));
+}
+
+function fetchUSA_Data() {
+    let validate = new Headers();
+    validate.append("X-CSCAPI-KEY", "UFVhZmZRcFNQM3luREtqTUhOS0lCMXVNOUQ2UHZ2R01VakNqZ3RtcA==");
+    // sets method to GET, checks if user has valid key, redirects to API url if valid
+    let requestOptions = {
+        method: "GET",
+        headers: validate,
+        redirect: "follow",
+    };
+
+    // Pulls selected data attribute from specific API Array
+    // template literal using country code to tell api which country data to pull
+    let fetchCountriesURL = `https://api.countrystatecity.in/v1/countries/${countryInit}`;
+
+    fetch(fetchCountriesURL, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            // sends result data to currency rate function
+            populateCountryData(result);
+        })
         .catch((error) => console.log("error", error));
 }
 
@@ -111,8 +159,6 @@ function populateCountryData(countryData) {
     fetch(fetchRatesURL)
         .then((response) => response.json())
         .then((result) => {
-            console.log(result);
-            console.log(countryData.currency_name);
             // following lines all populate specified divs with country information
             document.getElementById("showIso2Code").innerHTML = countryCode;
             document.getElementById("showCapital").innerHTML = capitalCity;
@@ -128,5 +174,7 @@ function populateCountryData(countryData) {
 // EVENTS
 // initial ping of country API to populate dropdown
 fetchCountries();
+fetchUSA_Flag();
+fetchUSA_Data();
 // country select from dropdown
 selectBar.addEventListener("change", fetchFlag);
